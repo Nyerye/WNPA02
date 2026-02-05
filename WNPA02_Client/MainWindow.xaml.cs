a03155b579861e73b0ab24125fa90fc4e200b272
Nyerye <nick_reilly@outlook.com>
2026-02-05 16:32:29 -0500

    Created the FetchSocket method. what this does is gets something usable with an IPv4 address. For now, I think its best to allow another method to take the TcpClient in and break it down for the specifics we need to connect.
    Created the ConnectToServer method that will use localhsot and the port for the server to make a connection with the server. From there, it will return an endpoint that Windows assigns to the player that can be stripped down to get the ip address and port. Stubbed out some extra bits for UI later on.

--- WNPA02_Client/MainWindow.xaml.cs
+++ WNPA02_Client/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,5 +22,49 @@ namespace WNPA02_Client
         {
             InitializeComponent();
         }
+
+        public static TcpClient FetchSocket()
+        {
+            //Fetch the player an empty socket that has an IPv4 address
+            TcpClient client = new TcpClient(AddressFamily.InterNetwork);
+
+            //Return it back. We will manipulate the data more in another method.
+            return client;
+            
+            
+        }
+        public static async Task ConnectToServer(TcpClient client)
+        {
+            //Need a way to talk to the server before we can get Windows to tell us the true IP and Port.
+            //However, everyones machine will be different if they are connecting to my server. 
+            //We know where the server lives, that wont change.
+            //Using localhost as the placeholder as it works on everyones machine.
+            IPAddress placeholder = IPAddress.Parse("127.0.0.1");
+            
+            //Try to connect the server and determine the IP and Port Windows sets for the client
+            try
+            {
+                
+                await client.ConnectAsync(placeholder, 6000);
+
+                //Break down the information we get from Windows.
+                IPEndPoint windowsAssigned = (IPEndPoint)client.Client.LocalEndPoint;
+
+                //Append the results for visibility
+                string message = $"Your IP Address is {windowsAssigned.Address}:{windowsAssigned.Port}";
+
+                //Maybe have some method that displays it on the client? Not there yet.
+
+                //Log the message
+                //Logger.Log(message);
+            }
+
+            catch (Exception ex)
+            {
+                //Take the exception and display it to the user to see. 
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
     }
 }
