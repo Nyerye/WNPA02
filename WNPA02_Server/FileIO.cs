3cbd00616caa79f6f8854ac4218ce4714143a741
Nyerye <nick_reilly@outlook.com>
2026-02-09 15:21:24 -0500

    -Made the FIleIO class. Probably will move the saving over to this one if feasable as it would be more appropriate but for usre, this is where the method for reading in games for the files will exist.

---
+++ WNPA02_Server/FileIO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WNPA02_Server
+{
+    internal class FileIO
+    {
+    }
+}
