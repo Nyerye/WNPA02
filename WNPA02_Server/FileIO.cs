021d5cbabde2f463659e44e8557342e3492f0f35
Nicholas Reilly <90353424+Nyerye@users.noreply.github.com>
2026-02-11 00:25:32 -0500

    -Added the FileIO class -Added the LoadrandomPuzzle method. As it stands, it employs the use of tuples. I do think that there should be a class thats maybe called Answerkey that would be created from this. Wrote email to Norbert.

--- WNPA02_Server/FileIO.cs
+++ WNPA02_Server/FileIO.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WNPA02_SharedClassLibrary;
 
 namespace WNPA02_Server
 {
@@ -15,6 +16,38 @@ namespace WNPA02_Server
         {
             "puzzle1.txt", "puzzle2.txt", "puzzle3.txt", "puzzle4.txt"
         };
+
+        public static (string puzzleString, int wordCount, HashSet<string> validWords) LoadRandomPuzzle()
+        {
+            //Pick a random index from the list.
+            int randomIndex = random.Next(files.Count);
+            string selectedFileName = files[randomIndex];
+
+            //Build the full path.
+            string filePath = Path.Combine(GameDirectory, selectedFileName);
+
+            //If the file does not exist, throw an exception.
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException($"Puzzle file not found: {filePath}");
+
+            //Read the file.
+            string[] lines = File.ReadAllLines(filePath);
+
+            string puzzleString = lines[0];
+            int wordCount = int.Parse(lines[1]);
+            HashSet<string> validWords = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+            //Add all words starting from line 3.
+            for (int i = 2; i < lines.Length; i++)
+            {
+                validWords.Add(lines[i].Trim());
+            }
+
+            //Write to the log it was successful.
+            UI.Log($"Loaded puzzle: {puzzleString} with {wordCount} words from {selectedFileName}");
+
+            return (puzzleString, wordCount, validWords);
+        }
 
     }
 }
