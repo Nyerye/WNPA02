f0be5fca7f2354918653f74daf9491118f4e2e8c
Nicholas Reilly <90353424+Nyerye@users.noreply.github.com>
2026-02-11 21:26:15 -0500

    Update Puzzle.cs
    -Added in the private and public data properties to the Puzzle class
    -Added in the constructor for the Puzzle class
    -Added in the IsValidGuess method, which can be used server side to evaluate the clients guess and see if its valid or not.

--- WNPA02_Server/Puzzle.cs
+++ WNPA02_Server/Puzzle.cs
@@ -6,7 +6,88 @@ using System.Threading.Tasks;
 
 namespace WNPA02_Server
 {
-    internal class Puzzle
+    /// <summary>
+    /// Puzzle class. Object that will hold the read information from 1/4 random files and can be used by the program to reference client guesses
+    /// </summary>
+    public class Puzzle
     {
+        //Set the private versions of the data members and then create their public versions.
+        private string puzzlestring;
+        private int wordcount;
+        private HashSet<string> words;
+
+        public string PuzzleString
+        {
+            get
+            {
+                return puzzlestring;
+            }
+            set
+            {
+                puzzlestring = value;
+            }
+        }
+
+        public int WordCount
+        {
+            get
+            {
+                return wordcount; 
+            }
+
+            set
+            {
+                wordcount = value;
+            }
+        }
+
+        public HashSet<string> Words
+        {
+            get
+            {
+                return words;
+            }
+
+            set
+            {
+                words = value;
+            }
+        }
+
+        /// <summary>
+        /// Constructor for the Puzzle class.
+        /// </summary>
+        /// <param name="puzzleString"></param>
+        /// <param name="wordCount"></param>
+        /// <param name="words"></param>
+        public Puzzle(string puzzleString, int wordCount, HashSet<string> words)
+        {
+            this.puzzlestring = puzzleString;
+            this.wordcount = wordCount;
+            this.words = words;
+        }
+
+        /// <summary>
+        /// Method that determines whether a guess is valid or not.
+        /// </summary>
+        /// <param name="guess"></param>
+        /// <param name="puzzle"></param>
+        /// <returns>
+        /// The guess in its lower case form if its valid, false if its empty or whitespace.
+        /// </returns>
+        public static bool IsValidGuess(string guess, Puzzle puzzle)
+        {
+            if (string.IsNullOrWhiteSpace(guess))
+            {
+                return false;
+            }
+            else
+            {
+                //Returns the lowercase word to our case insensitive hashset with any whitespasce trimmed off.
+                return puzzle.Words.Contains(guess.Trim().ToLower());
+            }
+            
+        }
+
     }
 }
